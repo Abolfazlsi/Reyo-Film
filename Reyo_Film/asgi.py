7639f72336f4bd1844b82d8807ea5f0f4e6128d1
Abolfazlsi <dge51547@gmail.com>
2024-09-13 22:36:58 +0330

    Create project Reyo Film.

---
+++ Reyo_Film/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Reyo_Film project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reyo_Film.settings')
+
+application = get_asgi_application()
